60bfa8f7835a7a58d786208c473e1883430b0275
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2025-11-10 15:25:06 +0530

    added apis under academics and facility

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -34,7 +34,12 @@ import { Auditorium } from "./collections/Auditorium";
 import { Atl } from "./collections/Atl";
 import { Library } from "./collections/Library";
 import { Smartclass } from "./collections/Smartclass";
-import { Event } from "./collections/Event";
+import { Preprimary } from "./collections/Preprimary";
+import { Secondary } from "./collections/Secondary";
+import { SeniorSecondary } from "./collections/SeniorSecondary";
+import { YearPlanandCalender } from "./collections/YearPlanandCalender";
+import { Result } from "./collections/Result";
+import { Clubs } from "./collections/Clubs";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -95,7 +100,12 @@ export default buildConfig({
     Atl,
     Library,
     Smartclass,
-    Event,
+    Preprimary,
+    Secondary,
+    SeniorSecondary,
+    YearPlanandCalender,   
+    Result,
+    Clubs
   ],
   
   // Add the import-export plugin with additional config to help with compatibility
